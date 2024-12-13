c035e9583cfa49701bb19039753cc6d9af1079c0
FatimadelaIglesia <fatimaiglesiav@gmail.com>
2024-12-13 17:20:41 +0000

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -5,13 +5,17 @@ setInterval(function () {
   let losAngelesTime = moment().tz("America/Los_Angeles");
   losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM, Do, YYYY");
   losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss[<small>]A[</small>]"
+    "h:mm:ss: SSS[<small>]A[</small>]"
   );
 }, 1000);
 
-let sydneyElement = document.querySelector("#sydney");
-let sydneyDateElement = sydneyElement.querySelector(".date");
-let sydneyTimeElement = sydneyElement.querySelector(".time");
-let sydneyTime = moment().tz("Australia/Sydney");
-sydneyDateElement.innerHTML = sydneyTime.format("MMMM, Do, YYYY");
-sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss[<small>]A[</small>]");
+setInterval(function () {
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM, Do, YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss: SSS[<small>]A[</small>]"
+  );
+}, 1000);
